d4f3eaaa8c30476e029bddb347fd22bfa3d85dd1
Brunerie <mb936701@iut21.u-bourgogne.fr>
2022-06-14 13:34:25 +0200

    ajout image

--- src/main/java/fr/brunerie/projet/application/accueilController.java
+++ src/main/java/fr/brunerie/projet/application/accueilController.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
